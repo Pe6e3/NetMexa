186361bb0773c98da9e0d221fc5508c1842b5e9a
Pe6e3 <xpe6e3@gmail.com>
2023-04-11 22:08:55 +0600

    Разделил все на части страниц, проименовал Сделал страницы: Обо мне, Мой кабинет, Отзызы, Мои работы, Контакты. Немного начал заполнять их, сделал неподходящие, но индивидуальные фото

---
+++ NetMexa/Views/Home/AboutPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NetMexa.Views.Home
+{
+    public class AboutPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ NetMexa/Views/Shared/SocialPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NetMexa.Views.Shared
+{
+    public class SocialPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
